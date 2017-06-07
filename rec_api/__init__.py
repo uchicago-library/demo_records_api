e27df1afa562aff393f5fa69af4b48c012d524c6
brian <balsamo@uchicago.edu>
2017-06-07 16:15:21 -0500

    Moving var values to env vars, rather than a file

--- rec_api/__init__.py
+++ rec_api/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from .blueprint import BLUEPRINT
+from flask_env import MetaFlaskEnv
+
+
+class Configuration(metaclass=MetaFlaskEnv):
+    ENV_PREFIX = "REC_API_"
+
+    DEBUG = False
+    DEFER_CONFIG = False
 
 app = Flask(__name__)
 
-app.config.from_envvar("REC_API_CONFIG", silent=True)
+app.config.from_object(Configuration)
 
 app.register_blueprint(BLUEPRINT)
